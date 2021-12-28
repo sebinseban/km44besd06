fb86ff89f2873fd72590001dd5faf1054386ff77
Sebin Sebastian <mailmesebin00@gmail.com>
2021-12-28 18:43:21 +0530

    modified simple interest program

--- C Basics/Variables/3_simple_interest.c
+++ C Basics/Variables/3_simple_interest.c
@@ -5,7 +5,7 @@ int main()
     float r,si,m;
     printf("Enter the principle amount, time in months and the rate of interest:");
     scanf("%d%d%f", &p,&n,&r);
-    m = n/120.0;
+    m = n/12.0;
     si = (p*m*r)/100 ;
     printf("The simple interest for %d months is %f \n", n , si);
     return 0;
