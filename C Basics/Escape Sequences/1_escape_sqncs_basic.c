7bea2190af3027c6ba066b1a388eea493f72ad5d
Sebin Sebastian <mailmesebin00@gmail.com>
2021-12-28 22:30:54 +0530

    Assignment 2, first question

---
+++ C Basics/Escape Sequences/1_escape_sqncs_basic.c
@@ -0,0 +1,19 @@
+//sebin sebastian
+//just using every escape character and explaining what each escape character does
+
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello, wor\b\bld\n"); // \b usage, the cursor will move back 2 times in this case
+    printf("Hi \a \n");           // \a will produce an audible alert, doesnt support on all machines
+    printf("Put a tab \t here\n");// \t will produce a tab (4 spaces)     \n is used for newline, will move the cursor to newline
+    printf("Clears all\rhello\n");     // \r will clear everything to the left of the cursor,replace with whats after \r, carriage return
+    printf("How does \f form feed works?\n");  // \f is similar to \v, will move text to the next line continuing from where it left off
+    printf("Is vertilcal tab \v same as form feed?\n");  //similar output of that while using \f. \v stands for vertical tab
+    printf("\'put this line in single qoutes\'\n"); // \' is how you produce a single qoute inside printf function
+    printf("\"put this line in double qoutes\"\n"); // \" is how you produce a double qoute inside printf function
+    printf("How to print a question mark like this \?\n");   // \? is how you produce a question mark inside printf function
+    //printf("You can print hexadecimal also via printf. Hexcode :\x11\n");
+    //printf("You can print octal also via printf. Octal: \nnn");
+}
