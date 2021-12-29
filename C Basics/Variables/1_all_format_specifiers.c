cf44fff8d344b07bbb8e59efe9b6b6fbb3e7e773
Sebin Sebastian <mailmesebin00@gmail.com>
2021-12-29 09:20:33 +0530

    Assignment 1, question 1

---
+++ C Basics/Variables/1_all_format_specifiers.c
@@ -0,0 +1,17 @@
+/*
+all format specifiers in c language
+
+int                             - %d
+unsigned int                    - %i
+short int                       - %hi
+short unsigned int              - %hu
+long signed int                 - %li
+long unsigned int               - %lu
+float                           - %f
+double                          - %lf
+long double                     - %Lf
+signed char                     - %c
+unsigned char                   - %c
+
+
+*/
