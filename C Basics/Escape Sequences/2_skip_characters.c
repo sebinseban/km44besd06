4f729fe8897c7478d755201e34aaf30f276151af
Sebin Sebastian <mailmesebin00@gmail.com>
2021-12-29 11:11:39 +0530

    update readme

--- C Basics/Escape Sequences/2_skip_characters.c
+++ C Basics/Escape Sequences/2_skip_characters.c
@@ -7,7 +7,7 @@
 int main()
 {
     //one way to implement skip character via scanf
-  /*  int date,month,year;
+    int date,month,year;
     printf("Enter your birthday seperated by comma :"); //the input will only be correctly accepted if you seperate by comma
     scanf("%d,%d,%d", &date,&month,&year);
     printf("Your birth date is %d-%d-%d\n", date,month,year);    
@@ -19,7 +19,7 @@ int main()
     printf("The three marks are %d, %d and %d.\n", mark1,mark2,mark3);
     
     //skipping characters with char variable
-*/
+
     char c;
     int new;
     printf("Enter a digit and a char: \n");
