3917c5a71b9e8b7a27b77a76e96eb7f4652591de
Sebin Sebastian <mailmesebin00@gmail.com>
2022-01-05 00:23:17 +0530

    Update 3rd question

--- C Basics/Logical Operators/3_leap_year_or_not.c
+++ C Basics/Logical Operators/3_leap_year_or_not.c
@@ -10,14 +10,12 @@ int main()
     printf("Enter a year:");
     scanf("%d",&year);
 
-    if( year % 100 == 0 && year % 400 == 0)
+    if( year % 400 == 0 || (year % 100 != 0 && year % 4 == 0) )
     {
         printf("Leap year.\n");
+        
     }
-    else if (year % 4 == 0)
-    {
-        printf("Leap year.\n");
-    }
+    
     else 
     {
         printf("Not leap year.\n");
---
+++ C Basics/Logical Operators/4_power_bill.c
@@ -0,0 +1,8 @@
+//sebin sebastian
+//05/02/2022
+//Find the power bill for the input number of units :
+/*
+        0 - 200 units : 100/- min
+        201 - 400 units : 100 + 0.65 per unit excess of 200
+        401 - 600 units : 230 + 0.80 per unit excess of 400
+        601 and above units : 390 + 1.00 per unit excess of 600 */
