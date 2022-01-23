0eb0418cc662ad5e4e1bbec092f7a651cc9aa3ff
Sebin Sebastian <mailmesebin00@gmail.com>
2022-01-23 23:50:54 +0530

    Add 10th question

---
+++ C Basics/10.Functions1/10_prime.c
@@ -0,0 +1,40 @@
+//sebin sebastian
+//23/01/2022
+/*Write a function that can take an integer as input and return 1, if the number is prime number 
+and 0 if it is not prime and print appropriate output message.
+        return type is integer.
+        IsPrime - returns int (0,1) */
+
+#include <stdio.h>
+
+int isPrime(int x)
+{
+    int count = 0;
+    for (int i = 1; i <= x; i++)
+    {
+        if (x % i == 0)
+            count++;
+    }
+    if (count == 2)
+        return 1;
+
+       
+    
+    else   
+        return 0;
+}
+
+int main()
+{
+    int x;
+    printf("Enter a number: ");
+    scanf("%d",&x);
+
+    if (isPrime(x) == 1)
+        printf("Prime.\n");
+
+    else
+        printf("Not prime.\n");
+    
+
+}
