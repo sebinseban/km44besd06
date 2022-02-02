9bfa051103978beba3360557512145a9b700aacd
Sebin Sebastian <mailmesebin00@gmail.com>
2022-02-02 10:45:08 +0530

    Add 10th assignment

--- C Basics/13.Arrays/10_duplicate_array.c
+++ C Basics/13.Arrays/10_duplicate_array.c
@@ -7,10 +7,11 @@
 
 int main()
 {
-    int size;
+    int size,count = 0;
     printf("Enter size of array: ");
     scanf("%d",&size);
     int arr[size];
+    int arr_new[10] = {};
 
     // Load values from user into the array
     printf("Array: ");
@@ -20,5 +21,57 @@ int main()
     }
     //printf("\n");
 
+    // Copy contents of original array to a duplicate array first
+    //no need
+    /*
+    for (int i = 0; i < size ; i++)
+    {
+        arr_new[i] = arr[i];
+    } */
+    /*just printing contents of newly created array
+    for (int i = 0 ; i < size ; i++)
+    {
+        printf("%d\t",arr_new[i]);
+    }  */
+  
+    // Store only duplicated values in the new array  eg: {1,2,2,2,3,3,4,5,5,5} => {2,3,5}
+
+
+
+    int compare_value = 0;
+    int j_counter = 0;
+
+    for (int i = 0 ; i < size ; i++)
+    {
+        
+        for (int j = 0; j < size ; j++)
+        {
+            
+            if ( compare_value == arr[j])
+            {
+                //printf("repeated at j =  %d\n",j);
+                count++;
+                
+            }
+            if (count == 2)
+            {
+                printf("repeated at j =  %d\n",j);
+                arr_new[j_counter] = arr[j];
+                j_counter++;
+                break;
+            }
+        }
+        
+        compare_value++;
+        count = 0;
+    }
+    
+    //print the new array with repeated elements
+    printf("New array with repeated elements only : ");
+    for (int i = 0 ; i < size ; i++)
+    {
+        printf("%d\t",arr_new[i]);
+    }
+    printf("\n");
     
 }
