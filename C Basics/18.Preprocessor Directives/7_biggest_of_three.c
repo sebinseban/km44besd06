0ab749445f52e1cf0c0fe5325de429d7449179cd
Sebin Sebastian <mailmesebin00@gmail.com>
2022-02-15 00:19:28 +0530

    Update logic for 7th

--- C Basics/18.Preprocessor Directives/7_biggest_of_three.c
+++ C Basics/18.Preprocessor Directives/7_biggest_of_three.c
@@ -5,27 +5,19 @@
 
 #include <stdio.h>
 
-#define max_of_three(a,b,c) int array[] = {a,b,c};\
-    int max = array[0];\
-    int length = sizeof (array) / sizeof(array[0]);\
-    for (int i = 0; i < length ; i++)\
-    {\
-        if (array[i] > max)\
-        {\
-            max = array[i];\
-        }\
-    }\
+//give the %d also as an arguement inside macro name <mn>
+//very important to learn to pass in format specifier alsos as arguement
+//this is only possible with stringizing operator
+#define max_of_three(x,y,z,arg) printf("Biggest:" #arg "\n",x>y&&x>z?x:y>z?y:z)
 
 int main()
 {
-    int a,b,c;
-    printf("Enter three integers: ");
-    scanf("%d %d %d",&a,&b,&c);
-    
+	int x = 5, y = 6, z = 7;
+	float a = 1.2, b = 5.6, c = 7.99;
+	double p = 12.567, q = 234.345, r = 120.999;
+	max_of_three(x,y,z,%d);    //passing format specifier also as argument
+	max_of_three(a,b,c,%f);
+	max_of_three(p,q,r,%lf);
 
-    max_of_three(a,b,c)
 
-
-
-    printf("%d\n",max);
 }
