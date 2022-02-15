6d3bea00b30503f1c29c5a67998849d43061cd62
Sebin Sebastian <mailmesebin00@gmail.com>
2022-02-15 11:22:43 +0530

    Update logic for 10th

--- C Basics/18.Preprocessor Directives/10_swap_generic.c
+++ C Basics/18.Preprocessor Directives/10_swap_generic.c
@@ -3,9 +3,6 @@
 //Write a macro to generate a generic function for swapping 2 numbers.
 
 #include <stdio.h>
-
-//int temp, a,b;
-
 #define  swapTwo(fun_name, datatype,x,y,format_specifier) datatype fun_name(datatype x, datatype y)\
                                     {\
                                         datatype temp = x;\
@@ -14,6 +11,11 @@
                                         printf(#x":"#format_specifier"\t"#y":"#format_specifier"\n",x,y);\
                                     }\
 
+swapTwo(intSwap,int,a,b,%d)
+swapTwo(floatSwap,float,c,d,%f)
+swapTwo(doubleSwap,double,e,f,%lf)
+
+//invoke the functions first that you are going to call
 int main()
 {
     //create some variables of different datatype
@@ -22,9 +24,10 @@ int main()
     double e = 5.999, f = 0.123;
 
     //macro call will name for macro,datatype,variables, and format specifier
-    swapTwo(intSwap,int,a,b,%d)
-    printf("%d %d",a,b);
-    swapTwo(floatSwap,float,c,d,%f)
-    swapTwo(doubleSwap,double,e,f,%lf)
+    //call the functions outside main function
+    intSwap(a,b);
+    floatSwap(c,d);
+    doubleSwap(e,f);
+    
     
 }
