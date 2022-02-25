15fcfdc2b259bc4ff12709a97dff8c72e24631de
Sebin Sebastian <mailmesebin00@gmail.com>
2022-02-25 05:13:35 +0000

    Update 12_function.c

---
+++ Embedded C/01.Bitwise Operators/12_function.c
@@ -0,0 +1,5 @@
+//sebin sebastian
+//24/02/2022
+//Write a functionsetbits (x,p,n,y)that returns x with then bits that begin at
+//position p set to the rightmost n bits of y, leaving the other bits unchanged.
+
