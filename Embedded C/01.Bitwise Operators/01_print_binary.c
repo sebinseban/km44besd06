109c5952621cc2428ad9418d28fc0c31edc89da5
Sebin Sebastian <mailmesebin00@gmail.com>
2022-02-25 04:48:03 +0000

    Upload New File

---
+++ Embedded C/01.Bitwise Operators/01_print_binary.c
@@ -0,0 +1,16 @@
+//sebin sebastian
+//24/02/2022
+//WAP to print binary
+
+int main()
+{
+	int a[4];
+	int number=6,i;
+	for(i=0;i<4;i++)
+	{
+		a[i]=number%2;
+		number=number/2;
+	}
+ 
+	return 0;
+}
