2f466c3219405d0bcfb3c0d1c79fc47bcfa71eaf
Sebin Sebastian <mailmesebin00@gmail.com>
2022-02-25 04:48:30 +0000

    Upload New File

---
+++ Embedded C/01.Bitwise Operators/03_sub_without.c
@@ -0,0 +1,18 @@
+//sebin sebastian
+//24/02/2022
+//WAP to implement subtraction functionality without using SUB ('-') Operator
+
+int main()
+{
+		int x = 10;
+		int y = 5;
+		while (y != 0) 
+    { 
+        int borrow = (~x) & y; 
+        x = x ^ y; 
+        y = borrow << 1; 
+    }
+	
+	return 0;
+	
+}
